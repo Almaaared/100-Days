4fcf0a2bc49a59f5161f93482b6f1db6b9df2fbe
Almaaared <Almaaared.techcampus@gmail.com>
2019-09-05 16:26:04 +1200

    Thaird comment 4

--- Thaird Week.py
+++ Thaird Week.py
@@ -13,4 +13,97 @@ Cherry
 >>> my_list[0]='blackcurrant'
 >>> print(my_list)
 ['blackcurrant', 'banana', 'cherry']
+>>> my_list = ['apple','banana','cherry']
+>>> del my_list[1]
+>>> print(my_list)
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> my_list = ['apple','banana','cherry']
+>>> del my_list[1]
+>>> print (my_list)
+['apple', 'cherry']
+>>> my_list = ['a','b','c','d']
+>>> print(my_list[1:3])
+['b', 'c']
+>>> lists=['apple','banana','cherry']
+>>> if'apple' in lists :
+	print('yes, 'apple' is in the fruite list')
+	
+SyntaxError: invalid syntax
+>>>  lists=['apple','banana','cherry']
+ 
+SyntaxError: unexpected indent
+>>> lists=['apple','banana','cherry']
+>>> if 'apple' in lists:
+	print ('yes, 'apple' in the frutes list')
+	
+SyntaxError: invalid syntax
+>>>  lists=['apple','banana','cherry']
+ 
+SyntaxError: unexpected indent
+>>> lists=['apple','banana','cherry']
+>>> if 'apple' in lists:
+print ('yes, "apple" in the frutes list')
+SyntaxError: expected an indented block
+>>> lists=['apple','banana','cherry']
+>>> if 'apple' in lists:
+print('yes, "apple" in the frutes list')
+SyntaxError: expected an indented block
+>>> lists=['apple','banana','cherry']
+>>> if 'apple' in lists:
+	print('yes, "apple" in the frutes list')
+
+	
+yes, "apple" in the frutes list
+>>> lists1['Ahmed','Mohii','Ali']
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    lists1['Ahmed','Mohii','Ali']
+NameError: name 'lists1' is not defined
+>>> lists 1=['Ahmed','Mohii','Ali']
+SyntaxError: invalid syntax
+>>> my_lists 1 =['Ahmed','Mohii','Ali']
+SyntaxError: invalid syntax
+>>> My_lists1 =['Ahmed','Mohii','Ali']
+>>> my_lists2 =["sara","Hind","Arwa"]
+>>> my_lists3 = My_lists1 + my_lists2
+>>> print(my_lists3)
+['Ahmed', 'Mohii', 'Ali', 'sara', 'Hind', 'Arwa']
+>>> this_list=['apple','banana','cherry']
+>>> this_list.append('orange')
+>>> print(this_list)
+['apple', 'banana', 'cherry', 'orange']
+>>> this_list=['apple','banana','cherry']
+>>> this_list.insert(1,'orange')
+>>> print(this_list)
+['apple', 'orange', 'banana', 'cherry']
+>>> my_list['apple','orange','cherry']
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    my_list['apple','orange','cherry']
+TypeError: list indices must be integers or slices, not tuple
+>>> my_list= ['apple','orange','cherry']
+>>> my_list.remove('orange')
+>>> print(my_list)
+['apple', 'cherry']
+>>> lists =['wlecome enytime','enyday']
+>>> my_list=lists.copy()
+>>> print(my_list)
+['wlecome enytime', 'enyday']
+>>> list_tuple =('apple','banana','cherry')
+>>> for x in list_tuple:
+	print(x)
+
+	
+apple
+banana
+cherry
+>>> 
+>>> my_list=('apple','orange','mango')
+>>> print(len(my_list))
+3
+>>> listis=[3,4,5,6]
+>>> my_list= tuple(listis)
+>>> print(my_list)
+(3, 4, 5, 6)
 >>> 
