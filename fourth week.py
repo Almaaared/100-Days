46bcebf40a312ffab677902429155f4773b9e550
Almaaared <Almaaared.techcampus@gmail.com>
2019-09-09 00:25:15 +1200

    fourth comment

---
+++ fourth week.py
@@ -0,0 +1,17 @@
+#thisset={'apple','banana','cherry'}
+#for x in thisset:
+    #print (x)
+
+#thisset={'apple','banana','cherry',1,2,3}
+#print (thisset)
+
+thisset ={'apple','banana','cherry'}
+thisset.add('orange')
+thisset.remove('apple')
+print (thisset)
+
+thisset={'apple','banana','cherry'}
+x= thisset.pop()
+print (x)
+print (thisset)
+
