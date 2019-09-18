4b8ba59c4753e5570624b2b55f0cf93df2f83761
Almaaared <Almaaared.techcampus@gmail.com>
2019-09-18 15:52:32 +1200

    FIVE comment 2

--- five Week.py
+++ five Week.py
@@ -1,7 +1,64 @@
-a = 33
+#a = 33
 
-b = 200
+#b = 200
 
-if b > a:
-    print("b is greater than a")
-    
+#if b > a:
+   # print("b is greater than a")
+
+"""a = 33
+b = 33
+
+if b>a:
+    print("bis greatre than a")
+elif a==b:
+    print("a and b are equal")"""
+"""
+a = 2
+b = 330
+
+print("A") if a>b  else print("B")
+"""
+"""
+a = 200
+b =33
+c = 500
+if a>b and c>a:
+    print("Both conditions are True")
+"""
+"""
+a = 200
+b =33
+c = 500
+if a>b or a>c:
+    print("At least one of the conditions is True")
+    """
+"""
+x = 41
+if x > 10:
+    print("Above ten")
+    if x > 20:
+        print("Above 20")
+    else:
+        print("but not above 20")
+        """
+"""
+i = 1
+while i < 6:
+    print(i)
+    i+= 1
+"""
+""""
+i = 1
+while i < 6:
+    print(i)
+    if i ==3:
+        break
+    i+= 1
+"""
+i = 0
+while i < 6:
+
+    i +=1
+    if i ==3:
+        continue
+    print(i)
